4ae2e5cc5d8c60a6492b3a42c72a23c45a774bc4
Pranjali Pokharel <pranjali@ualberta.ca>
2020-01-30 21:50:46 -0700

    Fix bug with start button callback. Clean up stage dependencies

--- tetris-react/src/components/Tetris.js
+++ tetris-react/src/components/Tetris.js
@@ -65,7 +65,7 @@ const Tetris = () => {
                 <Display text="Level" />
               </div>
             )}
-          <StartButton onclick={StartGame} />
+          <StartButton callback={StartGame} />
         </aside>
       </StyledTetris>
     </StyledTetrisWrapper>
