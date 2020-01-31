8a9dc26697ff280b61c3f5b93026a4630de09305
Pranjali Pokharel <pranjali@ualberta.ca>
2020-01-30 21:47:54 -0700

    Set up the update of the stage with a tetromino

--- tetris-react/src/hooks/useStage.js
+++ tetris-react/src/hooks/useStage.js
@@ -1,8 +1,31 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { createStage } from '../gameHelper';
 
-export const useStage = () => {
+export const useStage = (player, resetPlayer) => {
   const [stage, setStage] = useState(createStage());
+  useEffect(() => {
+    const updateStage = prevStage => {
+      // Have to clear stage before updating
+      const newStage = prevStage.map(row =>
+        // Use for loop for improved perfomance
+        row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)),
+      );
 
+      // Draw the tetromino
+      player.tetromino.forEach((row, y) => {
+        row.forEach((value, x) => {
+          if (value !== 0) {
+            newStage[y + player.pos.y][x + player.pos.x] = [
+              value,
+              `${player.collided ? 'merged' : 'clear'}`,
+            ];
+          }
+        });
+      });
+      return newStage;
+    };
+
+    setStage(prev => updateStage(prev))
+  }, [player.collided, player.pos.x, player.pos.y, player.tetromino]);
   return [stage, useStage];
 }
