a9df8361181a971a732f1412299ac11cb2df6df1
Pranjali Pokharel <pranjali@ualberta.ca>
2020-02-04 13:18:41 -0700

    Set up drop interval so that the blocks fall on their own

--- tetris-react/src/components/Tetris.js
+++ tetris-react/src/components/Tetris.js
@@ -3,8 +3,11 @@ import { createStage, checkCollision } from '../gameHelper';
 
 import { StyledTetris, StyledTetrisWrapper } from './styles/StyledTetris';
 
+// Custom Hooks
+import { useInterval } from '../hooks/useInterval';
 import { usePlayer } from '../hooks/usePlayer';
 import { useStage } from '../hooks/useStage';
+import { useGameStatus } from '../hooks/useGameStatus';
 
 import Stage from './Stage';
 import Display from './Display';
@@ -15,7 +18,10 @@ const Tetris = () => {
   const [gameOver, setGameOver] = useState(false);
 
   const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
-  const [stage, setStage] = useStage(player, resetPlayer);
+  const [stage, setStage, rowsCleared] = useStage(player, resetPlayer);
+  const [score, setScore, rows, setRows, level, setLevel] = useGameStatus(
+    rowsCleared
+  );
 
   console.log('Re-Render YEEHAW');
 
@@ -29,6 +35,7 @@ const Tetris = () => {
     console.log("start our game yo");
     //Reset Game
     setStage(createStage());
+    setDropTime(1000);
     resetPlayer();
     setGameOver(false);
   }
@@ -47,7 +54,15 @@ const Tetris = () => {
     }
   }
 
+  const keyUp = ({ keyCode }) => {
+    if (!gameOver) {
+      if (keyCode === 40) {
+        setDropTime(1000);
+      }
+    }
+  }
   const dropPlayer = () => {
+    setDropTime(null);
     drop();
   }
 
@@ -65,8 +80,12 @@ const Tetris = () => {
     }
   }
 
+  useInterval(() => {
+    drop();
+  }, dropTime)
+
   return (
-    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
+    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)} onKeyUp={keyUp}>
       <StyledTetris>
         <Stage stage={stage} />
         <aside>
